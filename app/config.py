2313f128c6caba989b679d6d633c7b2b45d6fc39
Dmitry Samofalov <samdimus@gmail.com>
2025-04-01 09:39:56 +0300

    add default value for debug_mode field

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     bot_token: Annotated[SecretStr, Field(None)] = None
-    debug_mode: Annotated[bool, Field(False)] = None
+    debug_mode: Annotated[bool, Field(False)] = False
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
