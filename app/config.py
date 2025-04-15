2a1b32b5c09e281501f936759870032638bae4a2
Dmitry Samofalov <samdimus@gmail.com>
2025-04-15 17:16:26 +0300

    fix types and defaults for fields of the class

--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
-from typing import Annotated
+from typing import Optional
 
 from pydantic import Field, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    bot_token: Annotated[SecretStr, Field(None)] = None
-    debug_mode: Annotated[bool, Field(False)] = False
+    bot_token: Optional[SecretStr] = Field(default=None)
+    debug_mode: bool = Field(default=False)
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
